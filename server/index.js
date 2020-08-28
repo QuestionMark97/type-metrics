be5d27558d5e51ca8fea740fb2c2b2ff83a42cfd
Mark Fusco <32991975+QuestionMark97@users.noreply.github.com>
2020-08-28 15:55:58 -0400

    Created global server error handler and served static files

--- server/index.js
+++ server/index.js
@@ -1,6 +1,26 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
+// Serve static files in 'client/build' folder under '/build route'
+app.use('/build', express.static(path.resolve(__dirname, '../client/build')));
+
+// Serve client entrypoint
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, '../client/index.html')));
+
+// Global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unkown middleware error',
+    status: 500,
+    msg: { err: 'An unknown error occured' }
+  };
+  const errorObj = { ...defaultErr, ...err };
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.msg);
+});
+
+// Listen on port
 app.listen(PORT, () => console.log('Listening on port 3000'));
