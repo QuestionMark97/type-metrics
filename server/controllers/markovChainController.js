27d0fadd4614e93ea8cadf428ab290a76ef21790
Mark Fusco <32991975+QuestionMark97@users.noreply.github.com>
2020-08-28 16:36:27 -0400

    Created 'charMap' object for determining if characters are letters and vowels/consonants

---
+++ server/controllers/markovChainController.js
@@ -0,0 +1,3 @@
+const markovChainController = {};
+
+module.exports = markovChainController;
---
+++ server/helpers/charMap.js
@@ -0,0 +1,62 @@
+const charMap = {
+  vowels: {
+    'a': true,
+    'e': true,
+    'i': true,
+    'o': true,
+    'u': true
+  },
+  consonants: {
+    'b': true,
+    'c': true,
+    'd': true,
+    'f': true,
+    'g': true,
+    'h': true,
+    'j': true,
+    'k': true,
+    'l': true,
+    'm': true,
+    'n': true,
+    'p': true,
+    'q': true,
+    'r': true,
+    's': true,
+    't': true,
+    'v': true,
+    'w': true,
+    'x': true,
+    'y': true,
+    'z': true
+  }
+};
+
+// Determine if letter is a consonant
+charMap._isConsonant = function isConsonant(char) {
+  if (typof char !== 'string') return false;
+  return (this.consonants[char.toLowerCase()]) ? true : false;
+};
+
+// Determine if letter is a vowel
+charMap._isVowel = function isVowel(char) {
+  if (typeof char !== 'string') return false;
+  return (this.vowels[char.toLowerCase()]) ? true : false;
+};
+
+// Determine if character is a letter
+charMap.isLetter = function isLetter(char) {
+  if (typof char !== 'string') return false;
+  return (this.vowels[char.toLowerCase()] || this.consonants[char.toLowerCase()]) ? true : false;
+};
+
+// Classify character as a vowel or consonant
+charMap.classify = function classify(char) {
+  if (this._isVowel(char)) return 'VOWEL';
+  else if (this._isConsonant(char)) return 'CONSONANT';
+  else return 'NOT_LETTER';
+};
+
+module.exports = {
+  isLetter: charMap.isLetter.bind(charMap),
+  classify: charMap.classify.bind(charMap)
+};
