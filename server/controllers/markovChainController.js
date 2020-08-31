4d158ec065c58dcf94820bc8abc42b3d39921c67
Mark Fusco <32991975+QuestionMark97@users.noreply.github.com>
2020-08-30 22:17:30 -0400

    Repurposed corpus reading middleware for Markov chain creation

--- server/controllers/markovChainController.js
+++ server/controllers/markovChainController.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
+const MarkovChain = require('../classes/MarkovChain');
 
 const markovChainController = {};
 
-// Acquire text
-markovChainController.readCorpus = (req, res, next) => {
+// Acquire Markov chain for text generation
+markovChainController.getChain = (req, res, next) => {
   fs.readFile('../corpus/corpus.txt', { encoding: 'utf8', flag: 'r' }, (err, corpus) => {
-    if (err) return next({ log: "Something went wrong with 'readCorpus' middleware in markovChainController" });
-    res.local.corpus = corpus;
+    if (err) return next({ log: "Something went wrong with 'getChain' middleware in markovChainController" });
+    const englishChain = new MarkovChain(corpus);
+    res.local.chain = englishChain;
     return next();
   });
 };
