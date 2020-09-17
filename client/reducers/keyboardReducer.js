e9c55334885e6ee18a85d75add7f3bb07ebf38a6
Mark Fusco <32991975+QuestionMark97@users.noreply.github.com>
2020-09-16 22:08:30 -0400

    Handled all eslint warnings and errors

--- client/reducers/keyboardReducer.js
+++ client/reducers/keyboardReducer.js
@@ -4,73 +4,79 @@ import { keyCodeToMatPos, getKeySpeeds } from '../helpers/reducerHelpers';
 const key = 'transparent';
 const bkrnd = '#f8f8f8';
 const chr = '#666';
-const data = false;
+const popup = false;
 const initialState = {
   keyTimes: {},
   keyColors: [
     [
-      [key, key, key, key, data], [key, key, key, key, data], [key, key, key, key, data],
-      [key, key, key, key, data], [key, key, key, key, data], [key, key, key, key, data],
-      [key, key, key, key, data], [key, key, key, key, data], [key, key, key, key, data],
-      [key, key, key, key, data]
+      [key, key, key, key, popup], [key, key, key, key, popup], [key, key, key, key, popup],
+      [key, key, key, key, popup], [key, key, key, key, popup], [key, key, key, key, popup],
+      [key, key, key, key, popup], [key, key, key, key, popup], [key, key, key, key, popup],
+      [key, key, key, key, popup]
     ],
     [
-      [key, key, key, key, data], [key, key, key, key, data], [key, key, key, key, data],
-      [key, key, key, key, data], [key, key, key, key, data], [key, key, key, key, data],
-      [key, key, key, key, data], [key, key, key, key, data], [key, key, key, key, data]
+      [key, key, key, key, popup], [key, key, key, key, popup], [key, key, key, key, popup],
+      [key, key, key, key, popup], [key, key, key, key, popup], [key, key, key, key, popup],
+      [key, key, key, key, popup], [key, key, key, key, popup], [key, key, key, key, popup]
     ],
     [
-      [key, key, key, key, data], [key, key, key, key, data], [key, key, key, key, data],
-      [key, key, key, key, data], [key, key, key, key, data], [key, key, key, key, data],
-      [key, key, key, key, data]
+      [key, key, key, key, popup], [key, key, key, key, popup], [key, key, key, key, popup],
+      [key, key, key, key, popup], [key, key, key, key, popup], [key, key, key, key, popup],
+      [key, key, key, key, popup]
     ],
     [
-      [bkrnd, chr, bkrnd, chr, data]
+      [bkrnd, chr, bkrnd, chr, popup]
     ]
   ]
 };
 
 function keyboardReducer(parentState = {}, state = initialState, action = {}) {
-  const keyColors = [...state.keyColors];
-  let [i, j] = keyCodeToMatPos(action.payload);
   const { charTimes, textGenerator } = parentState;
+  const keyColors = [...state.keyColors];
 
   switch (action.subtype) {
-    case types.UNLOCK_CHARS:
+    case types.UNLOCK_CHARS: {
       textGenerator.getChars().forEach((char) => {
-        [i, j] = keyCodeToMatPos(char);
-        keyColors[i][j] = [bkrnd, chr, bkrnd, chr, data];
+        const [i, j] = keyCodeToMatPos(char);
+        keyColors[i][j] = [bkrnd, chr, bkrnd, chr, popup];
       });
-      return {  ...state, keyColors };
+      return { ...state, keyColors };
+    }
 
-    case types.HIGHLIGHT_KEY:
+    case types.HIGHLIGHT_KEY: {
+      const [i, j] = keyCodeToMatPos(action.payload);
       if (i >= 0) {
         keyColors[i][j][0] = chr;
         keyColors[i][j][1] = bkrnd;
       }
       return { ...state, keyColors };
+    }
 
-    case types.UNHIGHLIGHT_KEY:
+    case types.UNHIGHLIGHT_KEY: {
+      const [i, j] = keyCodeToMatPos(action.payload);
       if (i >= 0) {
-        keyColors[i][j][0] = keyColors[i][j][2]
-        keyColors[i][j][1] = keyColors[i][j][3]
+        const [,, color, background] = keyColors[i][j];
+        keyColors[i][j][0] = color;
+        keyColors[i][j][1] = background;
       }
       return { ...state, keyColors };
+    }
 
-    case types.RECALC_SPEED:
-        const blue = '#3498db';
-        const keySpeeds = getKeySpeeds(charTimes);
-        for (let char in keySpeeds) {
-          const [i, j] = keyCodeToMatPos(char)
-          const speed = keySpeeds[char].relSpeed;
-          const addon = 255 * (1 + speed) / 2;
-          const color = blue + parseFloat((255 * (1 + speed) / 2).toFixed()).toString(16);
-          keyColors[i][j] = [color, bkrnd, color, bkrnd, true];
-        }
+    case types.RECALC_SPEED: {
+      const blue = '#3498db';
+      const keySpeeds = getKeySpeeds(charTimes);
+      Object.entries(keySpeeds).forEach(([char, data]) => {
+        const [i, j] = keyCodeToMatPos(char);
+        const speed = data.relSpeed;
+        const color = blue + parseFloat(((255 * (1 + speed)) / 2).toFixed()).toString(16);
+        keyColors[i][j] = [color, bkrnd, color, bkrnd, true];
+      });
       return { ...state, keyTimes: keySpeeds };
+    }
 
-    default:
+    default: {
       return state;
+    }
   }
 }
 
