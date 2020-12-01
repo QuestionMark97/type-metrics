98506ff63615c8a9bbf3a0a42c7902f1712d7da2
Mark Fusco <32991975+QuestionMark97@users.noreply.github.com>
2020-12-01 13:06:43 -0500

    Created factory function for creating error objects

---
+++ server/errors/errorFactory.js
@@ -0,0 +1,38 @@
+function errorFactory(err, CRUDType, status = 500) {
+  const errObj = { status };
+  switch (CRUDType) {
+    case this.CREATE:
+      return {
+        ...errObj,
+        log: err,
+        msg: { err: 'There was a problem creating resource' }
+      };
+    case this.READ:
+      return {
+        ...errObj,
+        log: err,
+        msg: { err: 'There was a problem reading resource' }
+      };
+    case this.UPDATE:
+      return {
+        ...errObj,
+        log: err,
+        msg: { err: 'There was a problem updating resource' }
+      };
+    case this.DESTROY:
+      return {
+        ...errObj,
+        log: err,
+        msg: { err: 'There was a problem destroying resource' }
+      };
+    default:
+      return {};
+  }
+}
+
+errorFactory.CREATE = 'CREATE';
+errorFactory.READ = 'READ';
+errorFactory.UPDATE = 'UPDATE';
+errorFactory.DESTROY = 'DESTROY';
+
+module.exports = errorFactory;
